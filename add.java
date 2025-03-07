b363a6d0146dfab575e955f769aa824c089f85ad
Stoic2000 <ezhumalai14032000@gmail.com>
2025-03-07 11:56:30 +0530

    second commit

--- add.java
+++ add.java
@@ -4,7 +4,7 @@ public class add {
     public static void main(String[] args) {
         int a=10;
         int b=20;
-        System.out.println("The sum is: "+(a+b));
         System.out.println("The sum is: "+(a-b));
+        
     }
 }
